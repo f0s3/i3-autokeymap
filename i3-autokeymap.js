3df905bc44c3a09d1335bb3110ff907fb892a069
f0s3 <power.aerozoll@gmail.com>
2018-09-25 20:55:37 +0300

    rename variables so code looks cleaner

--- i3-autokeymap.js
+++ i3-autokeymap.js
@@ -1,13 +1,13 @@
 const config = require('./config/config')
 const exec = require('child_process').exec
 
-let focusedWorkspace = (array) => { return array.find(i => i.focused) }
+let focusedWorkspace = (array) => {return array.find(i => i.focused)}
 
 setInterval(() => {
-	exec('i3-msg -t get_workspaces', (error, stdOut, stdErr) => {
-		let workspaceActivityArray = JSON.parse(stdOut).map(k => {return {num: k.num, focused: k.focused}})
-		exec('xkblayout-state print %s', (error, stdOut, stdErr) => {
-			if (config[focusedWorkspace(workspaceActivityArray).num] !== stdOut) exec('xkblayout-state set +1')
+	exec('i3-msg -t get_workspaces', (stdIn, workspacesArrayUnhandled, stdErr) => {
+		let workspaceArray = JSON.parse(workspacesArrayUnhandled).map(k => {return {num: k.num, focused: k.focused}})
+		exec('xkblayout-state print %s', (stdIn, currentLayout, stdErr) => {
+			if (config[focusedWorkspace(workspaceArray).num] !== currentLayout) exec('xkblayout-state set +1')
 		})
 	})
 }, 150)
