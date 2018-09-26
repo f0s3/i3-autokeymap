5825fbf20c0a098f7ea59a9a13b47efaf3885528
Antony <Toxa23A@gmail.com>
2018-09-26 19:43:57 +0300

    Make it work as expected

--- i3-autokeymap.js
+++ i3-autokeymap.js
@@ -3,12 +3,39 @@ const exec = require('child_process').exec
 
 let focusedWorkspace = (array) => { return array.find(i => i.focused) }
 
-setInterval(() => {
-	exec('i3-msg -t get_workspaces', (error, stdOut, stdErr) => {
-		let workspaceActivityArray = JSON.parse(stdOut).map(k => {return {num: k.num, focused: k.focused}})
-		exec('xkblayout-state print %s', (error, stdOut, stdErr) => {
-			if (config[focusedWorkspace(workspaceActivityArray).num] !== stdOut) exec('xkblayout-state set +1')
-		})
+let prevWorkspace = 1;
+let currentWorkspace = 1;
+
+let layoutOrder = {}
+
+
+exec('setxkbmap -query | grep layout', (stdIn, keyboardInfo, stdErr) => {
+	let layoutInfo = keyboardInfo.replace('layout:', '').trim()
+	layoutInfo.split(/,/).forEach((k, i) => {
+		layoutOrder[k] = i;
 	})
-}, 150)
+
+	setInterval(() => {
+		exec('i3-msg -t get_workspaces', (error, curWork, stdErr) => {
+			let workspaceActivityArray = JSON.parse(curWork).map(k => { return { num: k.num, focused: k.focused } })
+			let currentWorkspace = focusedWorkspace(workspaceActivityArray).num
+			if (currentWorkspace == prevWorkspace) {
+				exec('./xkblayout-state print %s', (stdIn, currentLayout, stdErr) => {
+					if (currentLayout != config[currentWorkspace]) {
+						config[currentWorkspace] = currentLayout
+					}
+				})
+			} else {
+				exec('./xkblayout-state print %s', (stdIn, stdOut, stdErr) => {
+					if (stdOut != config[currentWorkspace]) {
+						exec(`./xkblayout-state set ${layoutOrder[config[currentWorkspace]]}`)
+					}
+				})
+				prevWorkspace = currentWorkspace
+			}
+
+		})
+	}, 150)
+})
+
 module.exports.focusedWorkspace = focusedWorkspace
