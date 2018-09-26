adfd2bfddaf18b5fc5b89b52e7f65a92bb003e95
f0s3 <power.aerozoll@gmail.com>
2018-09-26 22:27:29 +0300

    Beautify code and rename variables

--- i3-autokeymap.js
+++ i3-autokeymap.js
@@ -1,41 +1,27 @@
 const config = require('./config/config')
 const exec = require('child_process').exec
 
-let focusedWorkspace = (array) => { return array.find(i => i.focused) }
-
-let prevWorkspace = 1;
+let previousWorkspace = 1;
 let currentWorkspace = 1;
-
 let layoutOrder = {}
 
-
 exec('setxkbmap -query | grep layout', (stdIn, keyboardInfo, stdErr) => {
-	let layoutInfo = keyboardInfo.replace('layout:', '').trim()
-	layoutInfo.split(/,/).forEach((k, i) => {
-		layoutOrder[k] = i;
-	})
-
+	let layoutInfo = keyboardInfo.replace('layout:', '').trim().split(/,/).forEach((k, i) => {layoutOrder[k] = i})
 	setInterval(() => {
-		exec('i3-msg -t get_workspaces', (error, curWork, stdErr) => {
-			let workspaceActivityArray = JSON.parse(curWork).map(k => { return { num: k.num, focused: k.focused } })
-			let currentWorkspace = focusedWorkspace(workspaceActivityArray).num
-			if (currentWorkspace == prevWorkspace) {
+		exec('i3-msg -t get_workspaces', (error, fullWorkspaceInfo, stdErr) => {
+			let workspaceActivityArray = JSON.parse(fullWorkspaceInfo).map(k => { return { num: k.num, focused: k.focused } })
+			let currentWorkspace = workspaceActivityArray.find(i => i.focused).num
+			if (currentWorkspace == previousWorkspace) {
 				exec('./xkblayout-state print %s', (stdIn, currentLayout, stdErr) => {
-					if (currentLayout != config[currentWorkspace]) {
-						config[currentWorkspace] = currentLayout
-					}
+					if (currentLayout != config[currentWorkspace]) config[currentWorkspace] = currentLayout
+					
 				})
 			} else {
-				exec('./xkblayout-state print %s', (stdIn, stdOut, stdErr) => {
-					if (stdOut != config[currentWorkspace]) {
-						exec(`./xkblayout-state set ${layoutOrder[config[currentWorkspace]]}`)
-					}
+				exec('./xkblayout-state print %s', (stdIn, currentLayout, stdErr) => {
+					if (currentLayout != config[currentWorkspace]) exec(`./xkblayout-state set ${layoutOrder[config[currentWorkspace]]}`)
 				})
-				prevWorkspace = currentWorkspace
+				previousWorkspace = currentWorkspace
 			}
-
 		})
-	}, 150)
+	}, 100)
 })
-
-module.exports.focusedWorkspace = focusedWorkspace
