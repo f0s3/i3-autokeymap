ca78740d84995a2b3bf22acbd2b55266928af0cc
f0s3 <power.aerozoll@gmail.com>
2018-10-18 23:00:57 +0300

    Adapt to new config file

--- keymap.py
+++ keymap.py
@@ -1,7 +1,6 @@
 import os,time,json
 import subprocess as sp
 
-
 previousWorkspace = 1
 currentWorkspace = 1
 layoutOrder = {}
@@ -20,9 +19,9 @@ while(True):
     currentWorkspace = [i["num"] for i in json.loads(shell("i3-msg -t get_workspaces")) if i["focused"]][0]
     if currentWorkspace == previousWorkspace:
         currentLayout = shell("xkblayout-state print %s")
-        if currentLayout != config[currentWorkspace]: config[currentWorkspace] = currentLayout
+        if currentLayout != config["workspaces"][currentWorkspace]: config["workspaces"][currentWorkspace] = currentLayout
     else:
-        if shell("xkblayout-state print %s") != config[currentWorkspace]:
-            os.system("xkblayout-state set {}".format(layoutOrder[config[currentWorkspace]]))
+        if shell("xkblayout-state print %s") != config["workspaces"][currentWorkspace]:
+            os.system("xkblayout-state set {}".format(layoutOrder[config["workspaces"][currentWorkspace][currentWorkspace]]))
         previousWorkspace = currentWorkspace
     time.sleep(100/1000)
