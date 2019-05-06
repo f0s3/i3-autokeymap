3083383be8b2e9a3505adb9f8b42dd40d53603e7
f0s3 <power.aerozoll@gmail.com>
2019-05-06 19:58:33 +0300

    Remove adding to the config to keep SRP

--- arch-install.sh
+++ arch-install.sh
@@ -2,4 +2,4 @@
 
 pacaur -S xkblayout-state-git python-pip --noconfirm;
 pip3 install --user psutil;
-echo "exec_always --no-startup-id 'cd ~/.config/i3-autokeymap && python3 keymap.py'" >> ~/.config/i3/config
+# echo "exec_always --no-startup-id 'cd ~/.config/i3-autokeymap && python3 keymap.py'" >> ~/.config/i3/config
