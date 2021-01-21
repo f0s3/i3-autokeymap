c7bf50ab77a03bf843b4a1e64f5410d7579cd757
f0s3 <power.aerozoll@gmail.com>
2021-01-21 03:53:06 +0200

    WIP: implement basic layout switch for applications

--- keymap.py
+++ keymap.py
@@ -1,13 +1,26 @@
-import os, time, json, psutil
-import subprocess as sp
+import os, time, json
+from subprocess import DEVNULL, STDOUT, PIPE, run, check_call
 
 previousWorkspace = 1
 currentWorkspace = 1
 appLaunched = False
 layoutOrder = {}
 
+class Application:
+    def __init__(self, command, layout, status=False):
+        self.command = command
+        self.layout = layout
+        self.status = status
+
+applications_layouts = []
+
+# todo: unhardcode me and fill in from config!
+applications_layouts.append(Application('dmenu_run', 'us'))
+applications_layouts.append(Application('i3', 'us'))
+applications_layouts.append(Application('keymap.py', 'us'))
+
 def shell(cmd):
-    return sp.run(cmd, stdout=sp.PIPE, shell=True).stdout.decode("utf8")
+    return run(cmd, stdout=PIPE, shell=True).stdout.decode("utf8")
 
 keyboardInfo = shell("setxkbmap -query | grep layout | cut -c 13-").strip()
 for index, e in enumerate(keyboardInfo.split(",")):
@@ -16,21 +29,31 @@ for index, e in enumerate(keyboardInfo.split(",")):
 with open("./config/config.json", "r+") as f:
     config = eval(f.read())
 
-def programIsRunning(programName):
-    for process in psutil.process_iter():
-        if process.name() == programName:
-            return True
-    return False
+
+def get_running_programs():
+    user = "f0s3" # todo: unhardcode me
+
+    first_part = "ps -u {} -o cmd ".format(user)
+    last_part = " | grep -v grep"
+
+    for app in applications_layouts:
+        a = shell(first_part + " | grep '{}'".format(app.command) + last_part)
+        if a.isdigit():
+            app.status = True
+
+def is_application_running(app):
+    return app.status == True
+
+get_running_programs()
 
 while True:
+    if get_running_programs() != "":
+        currentLayout = shell("xkblayout-state print %s") # maybe we can move it to a global variable
+        for app in list(filter(is_application_running, applications_layouts)):
+            if currentLayout != app.layout:
+                os.system("xkblayout-state set {}".format(app.layout))
+
     currentWorkspace = [i["num"] for i in json.loads(shell("i3-msg -t get_workspaces")) if i["focused"]][0]
-    for program in config["programs"]:
-        if programIsRunning(list(program.keys())[0]):
-            os.system("xkblayout-state set {}".format(layoutOrder[list(filter(lambda x: list(program.keys())[0] in x, config["programs"]))[0][list(program.keys())[0]]]))
-            appLaunched = True
-        elif appLaunched:
-            appLaunched = False
-            currentWorkspace = currentWorkspace + 1
     if currentWorkspace == previousWorkspace:
         currentLayout = shell("xkblayout-state print %s")
         if currentLayout != config["workspaces"][currentWorkspace][currentWorkspace]: config["workspaces"][currentWorkspace][currentWorkspace] = currentLayout
@@ -39,3 +62,4 @@ while True:
             os.system("xkblayout-state set {}".format(layoutOrder[config["workspaces"][currentWorkspace][currentWorkspace]]))
         previousWorkspace = currentWorkspace
     time.sleep(100 / 1000)
+
